c015a6f9ef2780e4fa571a2b8dd9f5dabb7558e7
Abdul Basir <basir161261@gmail.com>
2021-03-26 09:29:51 +0600

    by AB

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAz4CEabSmiXnwekGZnjFfHbiDcTFq2FwU",
+    authDomain: "burj-al-arab-ab-60841.firebaseapp.com",
+    projectId: "burj-al-arab-ab-60841",
+    storageBucket: "burj-al-arab-ab-60841.appspot.com",
+    messagingSenderId: "365052354468",
+    appId: "1:365052354468:web:602a1b272fc22504feeb31"
+  };
+export default firebaseConfig;
